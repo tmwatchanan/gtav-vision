bf1b4fb4c51bc92e2c581ac3d91d436526fe9334
tmwatchanan <tmwatchanan@gmail.com>
2020-11-25 01:17:03 -0600

    Replace d3dshot with win32ui bitmap
    Doing it this way is the best way to capture screen. It does not directly capture the screenshot, but captures from a specific window. Even the window is occluded by other windows, this still works.
    ref: https://stackoverflow.com/questions/41785831/how-to-optimize-conversion-from-pycbitmap-to-opencv-image

--- bot.py
+++ bot.py
@@ -1,40 +1,60 @@
 import cv2
+import numpy as np
 import time
 
-import d3dshot
+import win32gui
+import win32ui
+import win32con
 
 from processing import detect_face
 
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
 
-WIDTH = 1280
-HEIGHT = 720
+def background_screenshot(hwnd, width, height, left=0, top=0):
+    wDC = win32gui.GetWindowDC(hwnd)
+    dcObj=win32ui.CreateDCFromHandle(wDC)
+    cDC=dcObj.CreateCompatibleDC()
+    dataBitMap = win32ui.CreateBitmap()
+    dataBitMap.CreateCompatibleBitmap(dcObj, width, height)
+    cDC.SelectObject(dataBitMap)
+    cDC.BitBlt((0,0), (width-left, height-top), dcObj, (left,top), win32con.SRCCOPY)
 
-GAME_WIDTH = 1280
-GAME_HEIGHT = 720
+    signedIntsArray = dataBitMap.GetBitmapBits(True)
+    img = np.fromstring(signedIntsArray, dtype='uint8')
+    img.shape = (height, width, 4)
 
-def display_image(img):
-    resized_img = cv2.resize(img, (WIDTH, HEIGHT))
-    cv2.imshow('DIP', resized_img)
+    dataBitMap.SaveBitmapFile(cDC, 'screenshot.bmp')
+    dcObj.DeleteDC()
+    cDC.DeleteDC()
+    win32gui.ReleaseDC(hwnd, wDC)
+    win32gui.DeleteObject(dataBitMap.GetHandle())
+    return cv2.cvtColor(img, cv2.COLOR_RGBA2RGB)
 
+def display_image(img, width=None, height=None):
+    if width is not None and height is not None:
+        img = cv2.resize(img, (width, height))
+    cv2.imshow('DIP', img)
 
 def main():
-    d = d3dshot.create(capture_output="numpy")
+    hwnd = win32gui.FindWindow(None, "FiveM - GTA FIVEM 1%")
     while True:
-        screen = d.screenshot(region=(0, 30, GAME_WIDTH, GAME_HEIGHT))
-
-        # screen = grab_screen(region=(0,40,GAME_WIDTH,GAME_HEIGHT+40))
-        screen = cv2.cvtColor(screen, cv2.COLOR_BGR2RGB)
-
         last_time = time.time()
-        
-        face_img = detect_face(screen)
 
-        display_image(face_img)
+        img = background_screenshot(hwnd, 1280, 780, left=8, top=32)
+        # face_img = detect_face(screen)
+
+        display_image(img)
+
+        print(f'loop took {round(time.time()-last_time, 3)} seconds.')
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
-        # print(f'loop took {round(time.time()-last_time, 3)} seconds.')
 
 
 if __name__ == "__main__":
     main()
+
+
+# useful links
+# https://stackoverflow.com/questions/41785831/how-to-optimize-conversion-from-pycbitmap-to-opencv-image
